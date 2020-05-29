1cbb8617a237a809671b86c3a715c2a66decfaa0
VisalamMurugan <visalam.murugan@gmail.com>
2020-05-29 11:41:26 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,5 @@
 public statiic void main()
 {
-system.out.println("HI i am master branch");
-  system.out.println("HI i am feature2");
-  system.out.println("HI i am feature1");
+system.out.println("HI i am master branch -- feaure 1 and feature 2");
+ 
 }
